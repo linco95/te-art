7d9a1c3e490c9bd88ed763d9a9f0697cb9549643
Andreas Kjellqvist <linco95@gmail.com>
2021-12-05 21:51:10 +0100

    Added loading of image

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 use std::env;
 
+use image::{DynamicImage, GenericImageView};
+
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let path = parse_config(&args);
     println!("Received path: {}", path);
 
-    read_image(path);
+    let img = read_image(path);
+    // The dimensions method returns the images width and height.
+    println!("dimensions {:?}", img.dimensions());
+
+    // The color method returns the image's `ColorType`.
+    println!("{:?}", img.color());
 }
 
-fn read_image(path: &str) -> &str {
-    path
+fn read_image(path: &str) -> DynamicImage {
+    image::open(path).unwrap()
 }
 
 fn parse_config(args: &[String]) -> &str {
