430d88d032739a27c0c0798fdcc7e76320d7ba66
Andreas Kjellqvist <linco95@gmail.com>
2021-12-05 21:46:46 +0100

    Added image path argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    let path = parse_config(&args);
+    println!("Received path: {}", path);
+
+    read_image(path);
+}
+
+fn read_image(path: &str) -> &str {
+    path
+}
+
+fn parse_config(args: &[String]) -> &str {
+    let path = &args[1];
+
+    path
 }
