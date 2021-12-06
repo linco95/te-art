0737b9638b75a224182b32093ee306e03aa87dac
Andreas Kjellqvist <linco95@gmail.com>
2021-12-06 02:01:01 +0100

    Added te color palette

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,7 @@ use image::{
 };
 
 fn main() -> std::result::Result<(), Box<dyn Error>> {
-    const SIZE: u32 = 64;
+    const SIZE: u32 = 128;
     let args: Vec<String> = env::args().collect();
 
     let path = parse_config(&args);
@@ -29,9 +29,10 @@ fn main() -> std::result::Result<(), Box<dyn Error>> {
 
 fn parse_image(img: DynamicImage, width: u32, height: u32) {
     let scaled_img = resize(&img, width, height, FilterType::Nearest);
+    scaled_img.save("output/scaled_image.png").ok();
     let pixels: Vec<&[u8]> = scaled_img.pixels().map(|pixel| pixel.channels()).collect();
 
-    let result_img = quantify_image(width as usize, height as usize, 8, pixels).unwrap();
+    let result_img = quantify_image(width as usize, height as usize, pixels).unwrap();
     println!("result img: {}", (result_img.len() as f32).sqrt());
 
     let raw_image: Vec<u8> = result_img
@@ -66,16 +67,46 @@ fn parse_config(args: &[String]) -> &str {
 fn quantify_image(
     width: usize,
     height: usize,
-    palette_size: u8,
     img: Vec<&[u8]>,
 ) -> Result<Vec<Rgb>, Box<dyn Error>> {
     println!("Quantifying image...");
+
+    let te_palette: Vec<Rgb> = Vec::from([
+        (1, 1, 1),
+        (172, 218, 148),
+        (255, 254, 231),
+        (147, 185, 238),
+        (216, 205, 225),
+        (199, 235, 224),
+        (246, 180, 81),
+        (249, 152, 131),
+        (170, 216, 232),
+        (255, 234, 150),
+        (250, 214, 222),
+        (216, 231, 53),
+        (171, 167, 125),
+        (200, 112, 112),
+        (201, 255, 146),
+        (230, 181, 219),
+        (219, 192, 163),
+    ])
+    .iter()
+    .map(|&c| Rgb {
+        red: c.0 as f64 / 255.0,
+        green: c.1 as f64 / 255.0,
+        blue: c.2 as f64 / 255.0,
+    })
+    .collect();
+
+    let palette_size = te_palette.len() as u8;
     // Create the output buffer and quantized palette index buffer
     let mut quantized_image = Matrix2d::new(width, height);
 
     // Build the quantization parameters, verify if accepting user input
     let mut conditions = Params::new();
     conditions.palette_size(palette_size);
+    conditions.dithering_level_auto(width as u32, height as u32, palette_size as usize);
+    conditions.palette(te_palette.clone())?;
     conditions.verify_parameters()?;
 
     // Convert the input image buffer from Rgb<u8> to Rgb<f64>
