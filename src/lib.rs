0ccf9a55d5d1a85462164ad2172d135db6e36f7e
Andreas Kjellqvist <linco95@gmail.com>
2021-12-07 01:22:44 +0100

    Refactored image quant

--- src/lib.rs
+++ src/lib.rs
@@ -1,42 +1,109 @@
-use std::error::Error;
+use std::{error::Error, path::Path};
 
 use rscolorq::{color::Rgb, spatial_color_quant, Matrix2d, Params};
 
 use image::{
     imageops::{resize, FilterType},
-    save_buffer, ColorType, DynamicImage, Pixel,
+    save_buffer, ColorType, DynamicImage, ImageBuffer, Pixel, Rgba,
 };
 
-pub fn parse_image(img: DynamicImage, width: u32, height: u32) {
-    let scaled_img = resize(&img, width, height, FilterType::Nearest);
-    scaled_img.save("output/scaled_image.png").ok();
-    let pixels: Vec<&[u8]> = scaled_img.pixels().map(|pixel| pixel.channels()).collect();
+pub fn quantizie_image(path: &str, dimensions: (u32, u32)) {
+    let path = Path::new(path);
 
-    let result_img = quantify_image(width as usize, height as usize, pixels).unwrap();
-    println!("result img: {}", (result_img.len() as f32).sqrt());
+    let img = read_image(path.to_str().unwrap());
 
-    let raw_image: Vec<u8> = result_img
+    let scaled_img = scale_image(img, dimensions);
+
+    let pixels = get_pixels(&scaled_img);
+
+    let te_palette: Vec<Rgb> = Vec::from([
+        [1, 1, 1],
+        [172, 218, 148],
+        [255, 254, 231],
+        [147, 185, 238],
+        [216, 205, 225],
+        [199, 235, 224],
+        [246, 180, 81],
+        [249, 152, 131],
+        [170, 216, 232],
+        [255, 234, 150],
+        [250, 214, 222],
+        [216, 231, 53],
+        [171, 167, 125],
+        [200, 112, 112],
+        [201, 255, 146],
+        [230, 181, 219],
+        [219, 192, 163],
+    ])
+    .iter()
+    .map(|&c| into_f64_rgb(&c))
+    .collect();
+
+    let quantified_image = quantify_image(
+        dimensions.0 as usize,
+        dimensions.1 as usize,
+        pixels,
+        te_palette,
+    )
+    .unwrap();
+
+    let raw_buffer = get_raw_buffer(&quantified_image.0);
+
+    save_image(
+        raw_buffer,
+        dimensions,
+        format!(
+            "./output/quantized_output_{}",
+            path.file_name().unwrap().to_str().unwrap(),
+        )
+        .as_str(),
+    );
+}
+
+fn scale_image(img: DynamicImage, dimensions: (u32, u32)) -> ImageBuffer<Rgba<u8>, Vec<u8>> {
+    resize(&img, dimensions.0, dimensions.1, FilterType::Nearest)
+}
+
+fn get_pixels(img: &ImageBuffer<Rgba<u8>, Vec<u8>>) -> Vec<&[u8]> {
+    img.pixels().map(|pixel| pixel.channels()).collect()
+}
+
+fn get_raw_buffer(pixels: &[Rgb]) -> Vec<u8> {
+    pixels
         .iter()
-        .flat_map(move |color| {
-            [
-                (color.red * 255.) as u8,
-                (color.blue * 255.) as u8,
-                (color.green * 255.) as u8,
-            ]
-        })
-        .collect();
+        .flat_map(move |pixel| into_u8_rgb(*pixel))
+        .collect()
+}
 
+fn into_f64_rgb(pixel: &[u8]) -> Rgb {
+    Rgb {
+        red: pixel[0] as f64 / 255.,
+        green: pixel[1] as f64 / 255.,
+        blue: pixel[2] as f64 / 255.,
+    }
+}
+
+fn into_u8_rgb(pixel: Rgb) -> [u8; 3] {
+    [
+        (pixel.red * 255.) as u8,
+        (pixel.blue * 255.) as u8,
+        (pixel.green * 255.) as u8,
+    ]
+}
+
+fn save_image(buffer: Vec<u8>, dimensions: (u32, u32), path: &str) {
     save_buffer(
-        "./output/quantized_output.png",
-        raw_image.as_slice(),
-        width as u32,
-        height as u32,
+        path,
+        buffer.as_slice(),
+        dimensions.0,
+        dimensions.1,
         ColorType::Rgb8,
     )
     .expect("Failed to save image buffer");
 }
 
-pub fn read_image(path: &str) -> DynamicImage {
+fn read_image(path: &str) -> DynamicImage {
+    // TODO: Add error handling?
     image::open(path).unwrap()
 }
 
@@ -44,37 +111,12 @@ fn quantify_image(
     width: usize,
     height: usize,
     img: Vec<&[u8]>,
-) -> Result<Vec<Rgb>, Box<dyn Error>> {
+    input_palette: Vec<Rgb>,
+) -> Result<(Vec<Rgb>, Matrix2d<u8>), Box<dyn Error>> {
     println!("Quantifying image...");
 
-    let te_palette: Vec<Rgb> = Vec::from([
-        (1, 1, 1),
-        (172, 218, 148),
-        (255, 254, 231),
-        (147, 185, 238),
-        (216, 205, 225),
-        (199, 235, 224),
-        (246, 180, 81),
-        (249, 152, 131),
-        (170, 216, 232),
-        (255, 234, 150),
-        (250, 214, 222),
-        (216, 231, 53),
-        (171, 167, 125),
-        (200, 112, 112),
-        (201, 255, 146),
-        (230, 181, 219),
-        (219, 192, 163),
-    ])
-    .iter()
-    .map(|&c| Rgb {
-        red: c.0 as f64 / 255.0,
-        green: c.1 as f64 / 255.0,
-        blue: c.2 as f64 / 255.0,
-    })
-    .collect();
+    let palette_size = input_palette.len() as u8;
 
-    let palette_size = te_palette.len() as u8;
     // Create the output buffer and quantized palette index buffer
     let mut quantized_image = Matrix2d::new(width, height);
 
@@ -82,18 +124,12 @@ fn quantify_image(
     let mut conditions = Params::new();
     conditions.palette_size(palette_size);
     conditions.dithering_level_auto(width as u32, height as u32, palette_size as usize);
-    conditions.palette(te_palette)?;
+    conditions.palette(input_palette)?;
     conditions.verify_parameters()?;
 
     // Convert the input image buffer from Rgb<u8> to Rgb<f64>
     let image = Matrix2d::from_vec(
-        img.iter()
-            .map(|&c| Rgb {
-                red: c[0] as f64 / 255.0,
-                green: c[1] as f64 / 255.0,
-                blue: c[2] as f64 / 255.0,
-            })
-            .collect(),
+        img.iter().map(|&c| into_f64_rgb(c)).collect(),
         width,
         height,
     );
@@ -102,7 +138,7 @@ fn quantify_image(
 
     spatial_color_quant(&image, &mut quantized_image, &mut palette, &conditions)?;
 
-    let result_pizels = quantized_image
+    let result_pixels = quantized_image
         .iter()
         .map(move |&color_index| {
             *palette
@@ -114,5 +150,5 @@ fn quantify_image(
                 .unwrap()
         })
         .collect();
-    Ok(result_pizels)
+    Ok((result_pixels, quantized_image))
 }
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,7 @@ mod config;
 
 use std::{env, error::Error, fs::create_dir, process};
 
-use image::GenericImageView;
-use teart::{parse_image, read_image};
+use teart::quantizie_image;
 
 use crate::config::Config;
 
@@ -16,17 +15,8 @@ fn main() -> std::result::Result<(), Box<dyn Error>> {
         process::exit(1);
     });
 
-    let img = read_image(config.path.as_str());
-
-    // The dimensions method returns the images width and height.
-    println!("dimensions: {:?}", img.dimensions());
-
-    // The color method returns the image's `ColorType`.
-    println!("Colortype: {:?}", img.color());
-    println!("Ensuring output folder exists");
     create_dir("output").ok();
-
-    parse_image(img, SIZE, SIZE);
+    quantizie_image(config.path.as_str(), (SIZE, SIZE));
 
     Ok(())
 }
