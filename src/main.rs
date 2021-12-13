5526d18d2e097976f70ca37bc167b9c7431e376e
Andreas Kjellqvist <linco95@gmail.com>
2021-12-13 22:10:49 +0100

    Added help text on running the program

--- src/main.rs
+++ src/main.rs
@@ -77,7 +77,45 @@ impl From<AppConfig> for ServerParams {
     }
 }
 
+const HELP_STR: &'static str = r#"
+╔╦╗╔═╗  ╔═╗╦═╗╔╦╗
+ ║ ║╣   ╠═╣╠╦╝ ║ 
+ ╩ ╚═╝  ╩ ╩╩╚═ ╩ 
+Developed by: Andreas Kjellqvist.
+Special thanks to Göran Hällgren, Fredrik Björeman for help with XML importer
+****************************************************
+Welcome to my TE Art project!
+The first time you run this program it will crash as you need to fill out the config file!
+To run the tool just update the config file (teart_cfg) and run it with path to the image you want to create. 
+After it is done converting the image it will provide a preview in output folder. 
+If the preview looks good you can use the XML Importer to import the reservations into TimeEdit. 
+Just select the xml payload file in output folder as import file.
+****************************************************
+The config file contains of the following parameters:
+# Username of the user importing the reservations
+login_name: String, 
+# The size of the final image, works best with increments of 2 (2, 4, 8, 16, 32, 64, 128)
+# (recommended max 128 as that's the most that fits in 24 hours)
+size: u32,              
+# The starting datetime for the reservations. Recommended to be 00:00. This will be converted to local timezone of the importing user
+start_datetime: String,
+# Reservation mode for the import
+reservation_mode: String,
+# Organization to do the reservations for
+org: String,
+# This object is added to all reservations. This is what is selected to view the painting. You could have multiple drawings in the same
+# TE database by having each drawing on a canvas object, and swich which canvas object is selected
+canvas_object: (String, String),
+# List of the color objects. These are expected to be colored in TimeEdit, in the same order as the list of solid colors, and with the
+# last object being uncolored (for grey color)
+color_objects: Vec<(String, String)>,
+# For xml_importer this should be timeedit
+auth_server: String,
+****************************************************
+"#;
+
 fn main() -> std::result::Result<(), Box<dyn Error>> {
+    println!("{}", HELP_STR);
     let cfg: AppConfig = confy::load_path("./teart_cfg")?;
 
     let args: Vec<String> = env::args().collect();
